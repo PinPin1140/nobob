8f94e35a6858c33fffc8606dd7d8b93858be8b4a
PinPin1140 <73947569+PinPin1140@users.noreply.github.com>
2021-11-15 16:46:51 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,20 +1,16 @@
 const allpayment = (prefix) => {
 return `PAYMENT BY ZEEONE OFC
 
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
-		
+*さ BANK*	
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 089516833542
+		• Arya
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 089516833542
 		• EL*****TI
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 089516833542
 		• ZE**** **C
 
 *さ PULSA*
